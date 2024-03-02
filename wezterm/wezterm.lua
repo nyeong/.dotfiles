acf4637d1d43db39aec632369ff4d885d7d1d45a
An Nyeong <me@annyeong.me>
2024-03-02 13:02:05 +0900

    feat: [wezterm] Monaspace와 네오둥근모를 함께 쓰도록 변경

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,36 +4,33 @@ local config = {}
 local harfbuzz_features = {
     'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig',
 }
+local NeoDunggeunmoFont = {
+    family = 'NeoDunggeunmo Code',
+    scale = 1.2
+}
 
 config.color_scheme = 'nord'
-config.font = wezterm.font({
-    family = 'MonaspiceNe NFM',
-    harfbuzz_features = harfbuzz_features,
-})
+config.line_height = 1.13
+config.font = wezterm.font_with_fallback {
+    NeoDunggeunmoFont,
+    {
+        family = 'MonaspiceNe NFM',
+        harfbuzz_features = harfbuzz_features,
+    },
+}
 config.font_rules = {
     {
-        italic = false,
-        intensity = 'Normal',
-        font = wezterm.font{
-            family = 'MonaspiceNe NFM',
-            weight = 'Regular',
-            harfbuzz_features = harfbuzz_features,
-        },
-    },
-    {
         intensity = 'Bold',
-        font = wezterm.font{
-            family = 'MonaspiceKr NFM',
-            weight = 'Black',
-            harfbuzz_features = harfbuzz_features,
+        font = wezterm.font_with_fallback {
+            { family = 'MonaspiceKr NFM', weight = 'Black', harfbuzz_features = harfbuzz_features },
+            NeoDunggeunmoFont,
         },
     },
     {
         italic = true,
-        font = wezterm.font{
-            family = 'MonaspiceRn NFM',
-            weight = 'Regular',
-            harfbuzz_features = harfbuzz_features,
+        font = wezterm.font_with_fallback {
+            { family = 'MonaspiceRn NFM', weight = 'Regular', harfbuzz_features = harfbuzz_features, },
+            NeoDunggeunmoFont,
         },
     },
 }
