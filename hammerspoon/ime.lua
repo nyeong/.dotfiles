b31b1f45adb22e202fe50839cf3aac1ff9ad447a
An Nyeong <me@annyeong.me>
2024-03-02 13:01:30 +0900

    feat: [hammerspoon] IME 파일 포매팅

--- hammerspoon/ime.lua
+++ hammerspoon/ime.lua
@@ -1,5 +1,5 @@
-function is_contains(target, list)
-    for i, v in pairs(list) do
+local function is_contains(target, list)
+    for _, v in pairs(list) do
         if string.find(target, v) then
             return true
         end
@@ -8,26 +8,22 @@ function is_contains(target, list)
 end
 
 local esc_bind
-function back_to_eng()
-	print(hs.keycodes.currentSourceID())
-    local targetForEnglish = {'iTerm', '터미널', 'Code', 'Terminal', 'WezTerm', 'Zed', 'Obsidian'}
-	-- esc의 원래 동작 보장. 참고 https://github.com/johngrib/johngrib.github.io/issues/130#issuecomment-699728202
-	esc_bind:disable()
-	hs.eventtap.keyStroke({}, 'escape')
-	esc_bind:enable()
+local function back_to_eng()
+    local targetForEnglish = {'iTerm', '터미널', 'Code', 'Terminal', 'WezTerm', 'Zed', 'Obsidian', 'Alacritty'}
+    -- esc의 원래 동작 보장. 참고 https://github.com/johngrib/johngrib.github.io/issues/130#issuecomment-699728202
+    esc_bind:disable()
+    hs.eventtap.keyStroke({}, 'escape')
+    esc_bind:enable()
 
-	local frontmostApplication = hs.application.frontmostApplication()
+    local frontmostApplication = hs.application.frontmostApplication()
 
-	if frontmostApplication and is_contains(frontmostApplication:name(), targetForEnglish) then
-		local inputEnglish = "com.apple.keylayout.ABC"
-		local inputSource = hs.keycodes.currentSourceID()
-		if not (inputSource == inputEnglish) then
-
-			-- hs.eventtap.keyStroke({}, 'left')
-			-- hs.eventtap.keyStroke({}, 'right')
-			hs.keycodes.currentSourceID(inputEnglish)
-		end
+    if frontmostApplication and is_contains(frontmostApplication:name(), targetForEnglish) then
+	local inputEnglish = "com.apple.keylayout.ABC"
+	local inputSource = hs.keycodes.currentSourceID()
+	if not (inputSource == inputEnglish) then
+	    hs.keycodes.currentSourceID(inputEnglish)
 	end
+    end
 end
 
 esc_bind = hs.hotkey.new({}, 'escape', back_to_eng):enable()
