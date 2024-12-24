7adef240191ce2dcb208e34e9ef2f2fa038235e4
An Nyeong <me@annyeong.me>
2024-12-24 15:20:16 +0900

    flake setup

--- flake.nix
+++ flake.nix
@@ -3,16 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs: {
-    "nyeong" = inputs.home-manager.lib.homeManagerConfiguration {
+  outputs = { self, nixpkgs, darwin, home-manager }@inputs: {
+    darwinConfigurations.nyeong = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-      homeDirectory = "/Users/nyeong";
-      stateVersion = "21.05";
-      configuration.imports = [ ./modules/darwin/home.nix ];
-    };
+      configuration = {
+        home.homeDirectory = "/Users/nyeong";
+        home.stateVersion = "23.05";
+        home.username = "nyeong";
+      };
+      modules = [
+          home-manager.darwinModules.home-manager
+         ./modules/darwin/home.nix
+      ];
+      };
   };
 }
