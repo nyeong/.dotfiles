1a17209880b251024fe523df46531e2888544b10
An Nyeong <me@annyeong.me>
2025-11-18 12:00:45 +0900

    oc-eyes bootloader 최적화

--- hosts/oc-eyes/configuration.nix
+++ hosts/oc-eyes/configuration.nix
@@ -7,16 +7,6 @@
     ../../modules
   ];
 
-  users.users.${palette.user.username} = {
-    isNormalUser = true;
-    extraGroups = ["wheel"];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHW38hDRPSN1QuPELEBOj5ex6mV9Iw69z6jJRdveibGE me@annyeong.me"
-    ];
-    shell = pkgs.zsh;
-  };
-  programs.zsh.enable = true;
-
   zramSwap.enable = true;
   networking.hostName = "oc-eyes";
   networking.domain = "subnet11141831.oc.oraclevcn.com";
@@ -24,8 +14,5 @@
   services.tailscale.enable = true;
 
   system.stateVersion = "25.11";
-
-  boot.loader.grub.configurationLimit = 2;
-  boot.tmp.cleanOnBoot = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
--- hosts/oc-eyes/hardware-configuration.nix
+++ hosts/oc-eyes/hardware-configuration.nix
@@ -1,16 +1,29 @@
 {modulesPath, ...}: {
   imports = [(modulesPath + "/profiles/qemu-guest.nix")];
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    device = "nodev";
+  boot.tmp.cleanOnBoot = true;
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = false;
+      efiSysMountPoint = "/boot";
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 2;
+      editor = false;
+    };
+    grub.enable = false;
   };
+
+  boot.initrd = {
+    availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
+    kernelModules = ["nvme"];
+  };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/84AA-1B9F";
     fsType = "vfat";
   };
-  boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "xen_blkfront"];
-  boot.initrd.kernelModules = ["nvme"];
   fileSystems."/" = {
     device = "/dev/mapper/ocivolume-root";
     fsType = "xfs";
