cf49fff566e587411a52b0c5360fd2aae22eb233
An Nyeong <me@annyeong.me>
2025-11-18 11:35:12 +0900

    Add cachces

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -37,7 +37,11 @@
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
       ];
-      trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      builders-use-substitutes = true;
     };
     gc = {
       automatic = true;
